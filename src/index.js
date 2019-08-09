7b6aec32c7ebbfd4f7c4d3443ffaa00450323c2f
Malik Felix <malikfelix50@gmail.com>
2019-08-09 19:28:27 -0400

    Felix Experience website

--- src/index.js
+++ src/index.js
@@ -5,18 +5,18 @@ import MakeElement from './Tools/MakeElement.js';
 
 
 
-function RenderSite(){
+/*function RenderSite(){
   let body = document.querySelector('body');
 
   console.log(body);
 
   let makeEle = new MakeElement;
 
-  let bodyContainer = makeEle.createEle('div','bodyContainer',[12,12,12,12],'bodyContain');  
-      bodyContainer.innerHTML = `<div> Hey, Welcome to PureJS </div>`;
+  let bodyContainer = makeEle.createEle('div','bodyContainer',[12,12,12,12],'bodyContain');
+      bodyContainer.innerHTML = `<div> Hey, Welcome to PureJS </div>`
 
-  
+
   body.append(bodyContainer);
 }
 
-RenderSite(); 
+RenderSite(); */
